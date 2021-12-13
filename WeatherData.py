5a2522185cac82d98b9e350af4848a397cc5f9d0
CaglaYu <caglasi@hotmail.com>
2021-12-13 09:56:20 +0100

    db

--- WeatherData.py
+++ WeatherData.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.preprocessing import  LabelEncoder,OneHotEncoder
+from sklearn.compose import ColumnTransformer
 
 #form the dataframe from csv file
 df=pd.read_csv("weather/pandas/1_intro/nyc_weather.csv")
@@ -15,11 +17,20 @@ print("{:.2f}".format(df["Humidity"].mean()))
 #change dataset
 df=pd.read_csv("weather/pandas/2_dataframe_basics/weather_data.csv")
 
-print(df[2:5]["event"])
+#print(df[2:5]["event"])
 
 dfc = pd.read_csv("1000_companies.csv")
-print(dfc.head)
+#print(dfc.head)
+X=dfc.iloc[:,:-1]
+y=dfc.iloc[:,4]
 
+X=dfc.iloc[:,:-1].values
+y=dfc.iloc[:,4].values
 
-
-
+lec=LabelEncoder()
+X[:,3]=lec.fit_transform(X[:,3])
+ct = ColumnTransformer([("State", OneHotEncoder(), [3])], remainder = 'passthrough')
+X = ct.fit_transform(X)
+print(X[:10])
+X=X[:,1:]
+print(X[:10])
